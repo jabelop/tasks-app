bc7ed759bedac5e95ba0996e40d0344e75bb11a7
jabelop <jabelop@hotmail.com>
2026-03-06 20:25:58 +0100

    Feature: Improved logger middleware and added user info and execution time.

--- src/libs/shared/infrastructure/middlewares/logger.middleware.ts
+++ src/libs/shared/infrastructure/middlewares/logger.middleware.ts
@@ -1,11 +1,21 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
+    const logger = new Logger();
     const data = req.body && Object.keys(req.body).length ? req.body : (req.params || {}); 
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url} ${JSON.stringify(data)}`);
+    const start = performance.now();
+    res.on('finish', () => {
+      const end = performance.now();
+      const time = new Date().toISOString()
+      const payload = JSON.stringify(data);
+      const {id, role} = (<any>req).user;
+      const roleName = role.name;
+      //const userId = (Object.keys(req));
+      logger.log(`[${time}] ${req.method} ${req.url} execution time: ${(end - start).toFixed(2)} ms, payload: ${payload} userId: ${id} role: ${roleName}`)
+    });
     next();
   }
 }
